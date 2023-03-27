ab00bcb10e920d80dfab6f7727c63457a4e1bc47
Sam Clementine Nabila <samclementinenabila@gmail.com>
2023-03-27 22:49:35 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 /*
  * File: 3-print_numbers.c
- * Auth: Brennan D Baraban
+ * Auth: Sam Clemantine
  */
 
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_numbers - Prints the numbers from 0-9.
